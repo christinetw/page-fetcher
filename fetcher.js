55a3fb9b62930444479a80bdd4c44694b8933252
Christine Lo <christine.lo.hayes@gmail.com>
2021-09-26 22:36:49 -0400

    fetcher v1

--- fetcher.js
+++ fetcher.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const fs = require('fs');
 
 const args = process.argv;
 const newArgs = args.slice(2);
@@ -7,5 +8,13 @@ const url = newArgs[0];
 const filename = newArgs[1];
 
 request(url, (error, response, body) => {
-  console.log(body);
+  fs.writeFile(filename, body, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    let len = body.length;
+    console.log(`Downloaded and saved ${len} bytes to ${filename}`);
+  })
+  
 });
